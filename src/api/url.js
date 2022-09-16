6e2c118ff8586837b6013b2fa64107f12121bd47
gxc <1003823149@qq.com>
2022-09-17 00:37:42 +0800

    添加url

--- src/api/url.js
+++ src/api/url.js
@@ -2,7 +2,11 @@ const baseURL = process.env.NODE_ENV === 'production' ?
 'http://127.0.0.1:3002' : 'http://127.0.0.1:3001';
 
 const HomePageUrl = '/home/page';
+
+const FormPageUrl = '/home/form';
+
 export {
     baseURL,
     HomePageUrl,
+    FormPageUrl,
 }
